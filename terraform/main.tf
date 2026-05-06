27944e862605167009f94f3ba436a704090f2fdb
saibhaskar337 <saibhaskar337@gmail.com>
2026-05-06 15:46:47 +0530

    initial project upload - app, helm, terraform, cloudbuild

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+# Create VPC Network
+resource "google_compute_network" "vpc" {
+  name                    = "devops-vpc"
+  auto_create_subnetworks = true
+}
+
+# Create GKE Cluster (without default node pool)
+resource "google_container_cluster" "gke" {
+  name     = "devops-cluster"
+  location = "asia-south1"
+
+  deletion_protection = false
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network = google_compute_network.vpc.name
+
+  networking_mode = "VPC_NATIVE"
+
+  node_config {
+    disk_type    = "pd-standard"
+    disk_size_gb = 30
+    machine_type = "e2-medium"
+  }
+}
+
+# Create Node Pool (custom config)
+resource "google_container_node_pool" "nodes" {
+  name     = "devops-node-pool"
+  cluster  = google_container_cluster.gke.name
+  location = "asia-south1"
+
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"   # 1 vCPU, 4GB RAM — minimum viable for GKE
+
+    disk_type    = "pd-standard"
+    disk_size_gb = 30            # GKE recommended minimum
+
+    image_type = "COS_CONTAINERD"
+  }
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = "project-41544a08-ca3f-4ea2-8ab"
+  region  = "asia-south1"
+  zone    = "asia-south1-a"
+}
